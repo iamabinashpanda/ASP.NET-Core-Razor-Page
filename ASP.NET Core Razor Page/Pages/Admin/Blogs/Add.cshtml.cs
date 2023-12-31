7151b1bc0816eb1091ecac3e47c22365ee1d1408
iamabinashpanda <abi97nash@gmail.com>
2023-12-31 15:05:28 +0530

    changes to Navigation Bar and Added New Razor Page for Blog

---
+++ ASP.NET Core Razor Page/Pages/Admin/Blogs/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP.NET_Core_Razor_Page.Pages.Admin.Blogs
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
