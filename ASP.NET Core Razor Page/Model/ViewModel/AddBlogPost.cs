4f39d2adc1ec2194190821feec704801f7f70f83
iamabinashpanda <abi97nash@gmail.com>
2023-12-31 15:41:45 +0530

    mapping bind property for adding blog post

---
+++ ASP.NET Core Razor Page/Model/ViewModel/AddBlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace ASP.NET_Core_Razor_Page.Model.ViewModel
+{
+    public class AddBlogPost
+    {
+        public String Heading { get; set; }
+        public String PageTitle { get; set; }
+        public String Content { get; set; }
+        public String ShortDescription { get; set; }
+        public String FeaturedImageUrl { get; set; }
+        public String UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+    }
+}
--- ASP.NET Core Razor Page/Pages/Admin/Blogs/Add.cshtml.cs
+++ ASP.NET Core Razor Page/Pages/Admin/Blogs/Add.cshtml.cs
@@ -1,3 +1,4 @@
+using ASP.NET_Core_Razor_Page.Model.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,14 @@ namespace ASP.NET_Core_Razor_Page.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public AddBlogPost AddBlogPostRequest { get; set; }
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {
+
         }
     }
 }
